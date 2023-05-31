b62ef95859e77b8db52e0f5208d49ba2a768d7b7
Nikolaos Orfandis <niorfani123@gmail.com>
2023-05-31 13:59:19 +0300

    Create CRUD

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 
+//Import the Model
+const Event = require('./models/eventModel')
+
 
 //Create Express
 const app = express()
+
+//Create Middlewear
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 
 //Route
@@ -14,9 +21,72 @@ app.get('/',(req, res) => {
 } )
 
 
+//Create an Event
+app.post('/event', async(req,res) => {
+   try {
+       const event = await Event.create(req.body)
+       res.status(200).json(event);
+   } catch (error) {
+       console.log(error.message);
+       res.status(500).json({message: error.message})
+   }
+})
 
 
+//Fetch Data
+app.get('/event', async(req, res) => {
+    try {
+        const event = await Event.find({});
+        res.status(200).json(event);
+        } catch (erro) {
+            res.status(500).json({message: error.message})
+        }
+    })
 
+    //Fetch Data by ID
+    app.get('/event/:id', async(req,res) =>{
+        try {
+            const {id}  = req.params;
+            const event = await Event.findById(id);
+        res.status(200).json(event);
+        } catch (error) {
+            res.status(500).json({message: error.message})
+        }
+    })
+
+    //Update Data
+    app.put('/event/:id', async(req,res) => {
+        try {
+    
+            const {id} = req.params;
+            const event = await Event.findByIdAndUpdate(id,req.body);
+            //We cannot find any event in database
+            if(!event){
+                return res.status(404).json({message: `Cannot find any event with ID ${id}`})
+            }
+            const updatedEvent = await Event.findById(id);
+            res.status(200).json(updatedEvent);
+    
+        } catch (error) {
+            res.status(500).json({message: error.message})
+        }
+    })
+
+
+//Delete Data
+app.delete('/events/:id', async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const event = await Event.findByIdAndDelete(id);
+        if(!event){
+            return res.status(404).json({message: `Cannot find any event with ID ${id}`})
+        }
+        res.status(200).json(event)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+})
 
 
 //connect to MongoDB
